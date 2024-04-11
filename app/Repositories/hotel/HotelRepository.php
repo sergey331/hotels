ac3980e682ac6c5c487eea9e5e178d0e5bc8aa75
merimelikyan <mmelikyan@truckindigital.com>
2024-04-11 11:21:55 +0400

    imp

--- app/Repositories/hotel/HotelRepository.php
+++ app/Repositories/hotel/HotelRepository.php
@@ -2,40 +2,6 @@
 
 namespace App\Repositories\hotel;
 
-use App\Models\HotelRooms;
-use App\Models\HotelService;
-use App\Repositories\AuthHelper;
-use App\Services\Hotel\HotelServices;
-
 class HotelRepository
 {
-    private HotelServices $hotelService;
-
-    public function __construct(HotelServices $hotelService)
-    {
-        $this->hotelService = $hotelService;
-    }
-
-    public function save(array $data)
-    {
-        $hotel = AuthHelper::getCurrentHotel();
-
-        $this->hotelService->setHotel($hotel);
-        $this->hotelService->setModel(HotelService::class);
-        foreach ($data['services'] as $s) {
-            $this->hotelService->setServiceData($s);
-            $this->hotelService->processService();
-            $this->hotelService->processImageService();
-        }
-        $this->hotelService->removeService();
-
-        $this->hotelService->setModel(HotelRooms::class);
-        foreach ($data['rooms'] as $r) {
-            $this->hotelService->setHotelRooms($r);
-            $this->hotelService->processRooms();
-            $this->hotelService->processRoomImages();
-        }
-
-        $this->hotelService->removeRooms();
-    }
 }
